8081dd140110c5f0ab2689e45f1ccbef3ed9b462
Marina Sereda <marina.gorb@gmail.com>
2022-09-10 12:27:57 +0300

    Finish Task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(
+      (item) =>
+        `<a class="gallery__item" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+          />
+        </a>`
+    )
+    .join("");
+
+  return markup;
+}
